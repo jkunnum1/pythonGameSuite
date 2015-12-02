5066e0604926857bcb226d67510d374ad6717962
William McLaughlin <wdmc85@gmail.com>
2015-12-02 15:36:33 -0500

    Colored and Styled some more
    Changed geometry manager for the LoginGUI to grid instead of pack
    Colored the GUIs

--- LoginGUI.py
+++ LoginGUI.py
@@ -1,5 +1,6 @@
 #tkinter
 import tkinter
+from tkinter import *
 import pickle
 import registration
 import RegisterGUI
@@ -11,46 +12,64 @@ class LoginGUI:
         self.__mainWindow = tkinter.Tk()
         # Set Window Title
         self.__mainWindow.wm_title("Login")
+        self.__mainWindow.configure(background="#004c66")
         # Create Sections:
-        self.__topFrame = tkinter.Frame(self.__mainWindow)
-        self.__middleFrame = tkinter.Frame(self.__mainWindow)
         self.__statusZone = tkinter.Frame(self.__mainWindow)
         self.__bottomFrame = tkinter.Frame(self.__mainWindow)
-        # Create Widgets For Top Frame:
-        self.__userLabel = tkinter.Label(self.__topFrame, text="Username:")
-        self.__userEntry = tkinter.Entry(self.__topFrame, width=10)
+        # Create Widgets:
+        self.__title = tkinter.Label(self.__mainWindow, text="Login to Play!",
+                                     font=("fixedsys", 15), fg="#ff9900")
+        self.__userLabel = tkinter.Label(self.__mainWindow, text="Username:",
+                                         font=("fixedsys", 10), fg="#ff9900")
+        self.__userEntry = tkinter.Entry(self.__mainWindow, width=13, bg="#c2e4f0",
+                                         font=("fixedsys", 10))
+        #Configure:
+        self.__title.configure(background="#004c66")
+        self.__userLabel.configure(background="#004c66")
         # Pack Widgets:
-        self.__userLabel.pack(side="left")
-        self.__userEntry.pack(side="left")
-        # Create Widgets For Middle Frame:
-        self.__passLabel = tkinter.Label(self.__middleFrame, text="Password:")
-        self.__passEntry = tkinter.Entry(self.__middleFrame, width=10)
+        self.__title.grid(row=0, columnspan=2, sticky=NSEW, pady=20)
+        self.__userLabel.grid(row=1, column=0, sticky=W, padx=20, pady=2)
+        self.__userEntry.grid(row=1, column=1, sticky=E, padx=20, pady=2)
+        # Create Widgets
+        self.__passLabel = tkinter.Label(self.__mainWindow, text="Password:",
+                                         font=("fixedsys", 10), fg="#ff9900")
+        self.__passEntry = tkinter.Entry(self.__mainWindow, width=13, bg="#c2e4f0",
+                                         font=("fixedsys", 10))
         # Configure Password Entry Widget:
-        self.__passEntry.config(show='*')
+        self.__passEntry.configure(show='*')
+        self.__passLabel.configure(background="#004c66")
         # Pack Widgets:
-        self.__passLabel.pack(side="left")
-        self.__passEntry.pack(side="left")
+        self.__passLabel.grid(row=2, column=0, sticky=W, padx=20, pady=2)
+        self.__passEntry.grid(row=2, column=1, sticky=E, padx=20, pady=2)
         # Create Login Status Zone:
         self.__result = tkinter.StringVar()
         self.__statusLabel = tkinter.Label(self.__statusZone,
                                       textvariable=self.__result)
+        self.__statusLabel.configure(background="#004c66", fg="#ff9900")
         # Pack Status
-        self.__statusLabel.pack(side="left")
+        self.__statusLabel.grid(row=3, padx=10, sticky=NSEW)
         
         # Create Buttons:
         self.__logButton = tkinter.Button(self.__bottomFrame, text="Login",
-                                          command=self.__login, relief="groove")
+                                          command=self.__login,
+                                          relief="groove", bg="#c2e4f0",
+                                          activebackground="#d6edf5", border=0,
+                                          font=("fixedsys", 6))
         self.__regButton = tkinter.Button(self.__bottomFrame, text="Register",
-                                          command=self.__register, relief="groove")
+                                          command=self.__register,
+                                          relief="groove", bg="#c2e4f0",
+                                          activebackground="#d6edf5", border=0,
+                                          font=("fixedsys", 6))
         # Pack Widgets:
-        self.__logButton.pack(side="left")
-        self.__regButton.pack(side="left")
+        self.__logButton.grid(row=4, column=0, sticky=E, padx=1)
+        self.__regButton.grid(row=4, column=1, sticky=W, padx=1)
+
+        self.__bottomFrame.configure(background="#004c66")
+        self.__statusZone.configure(background="#004c66")
 
         # Pack the frames(padx and pady add padding to the frames):
-        self.__topFrame.pack(padx=40, pady=10)
-        self.__middleFrame.pack(padx=40, pady=0)
-        self.__statusZone.pack()
-        self.__bottomFrame.pack(padx=40, pady=10)
+        self.__statusZone.grid(columnspan=2)
+        self.__bottomFrame.grid(columnspan=2, padx=7, pady=20)
 
         # Enter the main loop:
         tkinter.mainloop()
@@ -87,17 +106,3 @@ class LoginGUI:
 
     def __update(self):
         self.__users = pickle.load(open("users.dat", "rb"))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- RegisterGUI.py
+++ RegisterGUI.py
@@ -3,6 +3,7 @@
 import tkinter
 import pickle
 from tkinter import *
+#import tkinter.font as tkFont
 import verification
 
 class RegisterGUI:
@@ -10,112 +11,160 @@ class RegisterGUI:
         self.__users = users
         self.__mainWindow = tkinter.Tk()
         self.__mainWindow.wm_title("Register")
+        self.__mainWindow.configure(background="#004c66")
 
         self.__title = tkinter.Frame(self.__mainWindow)
         self.__buttons = tkinter.Frame(self.__mainWindow)
         
         self.__titleText = tkinter.Label(self.__mainWindow,
-                                         text="Register New User:")
-        self.__titleText.grid(row=0, columnspan=2, pady=10)
+                                         text="Register New User:",
+                                         font=("fixedsys", 15), fg="#ff9900")
+        self.__titleText.configure(background="#004c66")
+        self.__titleText.grid(row=0, columnspan=2, pady=20)
         # User frame widgets. Format for this and all others will be:
         # (Attribute) : input : status
-        self.__userLabel = tkinter.Label(self.__mainWindow, text="Username:")
-        self.__userEntry = tkinter.Entry(self.__mainWindow, width=20)
-        self.__userStatusLabel = tkinter.Label(self.__mainWindow, text = '')
+        self.__userLabel = tkinter.Label(self.__mainWindow, text="Username:",
+                                         font=("fixedsys", 10), fg="#ff9900")
+        self.__userEntry = tkinter.Entry(self.__mainWindow, width=13, bg="#c2e4f0",
+                                         font=("fixedsys", 10))
+        self.__userStatusLabel = tkinter.Label(self.__mainWindow, text = '', fg="#ff9900")
+
+        self.__userLabel.configure(background="#004c66")
+        self.__userStatusLabel.configure(background="#004c66")
 
         # grid Widgets:
-        self.__userLabel.grid(row=1, column=0, sticky=W, padx=7, pady=1)
-        self.__userEntry.grid(row=1, column=1, sticky=E, padx=10, pady=1)
+        self.__userLabel.grid(row=1, column=0, sticky=W, padx=20, pady=1)
+        self.__userEntry.grid(row=1, column=1, sticky=E, padx=20, pady=1)
         self.__userStatusLabel.grid(row=2, column=1)
 
+
         # Create Password Widgets:
-        self.__passLabel = tkinter.Label(self.__mainWindow, text="Password:")
-        self.__passEntry = tkinter.Entry(self.__mainWindow, width=20)
-        self.__placeholder = tkinter.Label(self.__mainWindow, text='')
+        self.__passLabel = tkinter.Label(self.__mainWindow, text="Password:",
+                                         font=("fixedsys", 10), fg="#ff9900")
+        self.__passEntry = tkinter.Entry(self.__mainWindow, width=13, bg="#c2e4f0",
+                                         font=("fixedsys", 10))
+        self.__placeholder = tkinter.Label(self.__mainWindow, text='', fg="#ff9900")
 
         # Configure Password Entry Widget:
         self.__passEntry.config(show='*')
+        self.__passLabel.configure(background="#004c66")
+        self.__placeholder.configure(background="#004c66")
         
         # grid Widgets:
-        self.__passLabel.grid(row=3, column=0, sticky=W, padx=7, pady=1)
-        self.__passEntry.grid(row=3, column=1, sticky=E, padx=10, pady=1)
+        self.__passLabel.grid(row=3, column=0, sticky=W, padx=20, pady=1)
+        self.__passEntry.grid(row=3, column=1, sticky=E, padx=20, pady=1)
         self.__placeholder.grid(row=4)
 
         # Create Confirm Password Widgets:
         self.__confirmPassLabel = tkinter.Label(self.__mainWindow,
-                                                text="Confirm Password:")
-        self.__confirmPassEntry = tkinter.Entry(self.__mainWindow,
-                                                width=20)
+                                                text="Confirm Password:",
+                                                font=("fixedsys", 10),
+                                                fg="#ff9900")
+        self.__confirmPassEntry = tkinter.Entry(self.__mainWindow, width=13, bg="#c2e4f0",
+                                         font=("fixedsys", 10))
         self.__confirmPassStatusLabel = tkinter.Label(self.__mainWindow,
-                                                      text='')
+                                                      text='', fg="#ff9900")
 
         # Configure Password Entry Widget:
         self.__confirmPassEntry.config(show='*')
+        self.__confirmPassLabel.configure(background="#004c66")
+        self.__confirmPassStatusLabel.configure(background="#004c66")
+
         # grid Widgets:
-        self.__confirmPassLabel.grid(row=5, column=0, sticky=W, padx=7, pady=1)
-        self.__confirmPassEntry.grid(row=5, column=1, sticky=E, padx=10, pady=1)
+        self.__confirmPassLabel.grid(row=5, column=0, sticky=W, padx=20, pady=1)
+        self.__confirmPassEntry.grid(row=5, column=1, sticky=E, padx=20, pady=1)
         self.__confirmPassStatusLabel.grid(row=6, column=1)
 
         # Create Name Input:
         self.__firstNameLabel = tkinter.Label(self.__mainWindow,
-                                              text="First Name:")
-        self.__firstNameEntry = tkinter.Entry(self.__mainWindow,
-                                              width=20)
+                                              text="First Name:",
+                                              font=("fixedsys", 10),
+                                              fg="#ff9900")
+        self.__firstNameEntry = tkinter.Entry(self.__mainWindow, width=13, bg="#c2e4f0",
+                                         font=("fixedsys", 10))
         self.__firstNameStatusLabel = tkinter.Label(self.__mainWindow,
-                                                    text='')
+                                                    text='', fg="#ff9900")
+
+        self.__firstNameLabel.configure(background="#004c66")
+        self.__firstNameStatusLabel.configure(background="#004c66")
 
         # grid Widgets:
-        self.__firstNameLabel.grid(row=7, column=0, sticky=W, padx=7, pady=1)
-        self.__firstNameEntry.grid(row=7, column=1, sticky=E, padx=10, pady=1)
+        self.__firstNameLabel.grid(row=7, column=0, sticky=W, padx=20, pady=1)
+        self.__firstNameEntry.grid(row=7, column=1, sticky=E, padx=20, pady=1)
         self.__firstNameStatusLabel.grid(row=8, column=1)
 
         # Create Last Name Input:
         self.__lastNameLabel = tkinter.Label(self.__mainWindow,
-                                              text="Last Name:")
-        self.__lastNameEntry = tkinter.Entry(self.__mainWindow,
-                                              width=20)
+                                              text="Last Name:",
+                                             font=("fixedsys", 10),
+                                             fg="#ff9900")
+        self.__lastNameEntry = tkinter.Entry(self.__mainWindow, width=13, bg="#c2e4f0",
+                                         font=("fixedsys", 10))
         self.__lastNameStatusLabel = tkinter.Label(self.__mainWindow,
-                                                   text='')
+                                                   text='', fg="#ff9900")
+        
+
+        self.__lastNameLabel.configure(background="#004c66")
+        self.__lastNameStatusLabel.configure(background="#004c66")
 
         # grid Widgets:
-        self.__lastNameLabel.grid(row=9, column=0, sticky=W, padx=7, pady=1)
-        self.__lastNameEntry.grid(row=9, column=1, sticky=E, padx=10, pady=1)
+        self.__lastNameLabel.grid(row=9, column=0, sticky=W, padx=20, pady=1)
+        self.__lastNameEntry.grid(row=9, column=1, sticky=E, padx=20, pady=1)
         self.__lastNameStatusLabel.grid(row=10, column=1)
         
         # Create email Input:
         self.__emailLabel = tkinter.Label(self.__mainWindow,
-                                              text="Email:")
-        self.__emailEntry = tkinter.Entry(self.__mainWindow,
-                                              width=20)
-        self.__emailStatusLabel = tkinter.Label(self.__mainWindow, text='')
+                                          text="Email:", font=("fixedsys",
+                                                               10),
+                                          fg="#ff9900")
+        self.__emailEntry = tkinter.Entry(self.__mainWindow, width=13, bg="#c2e4f0",
+                                          font=("fixedsys", 10))
+        self.__emailStatusLabel = tkinter.Label(self.__mainWindow, text='', fg="#ff9900")
+
+        self.__emailLabel.configure(background="#004c66")
+        self.__emailStatusLabel.configure(background="#004c66")
 
         # grid Widgets:
-        self.__emailLabel.grid(row=11, column=0, sticky=W, padx=7, pady=1)
-        self.__emailEntry.grid(row=11, column=1, sticky=E, padx=10, pady=1)
+        self.__emailLabel.grid(row=11, column=0, sticky=W, padx=20, pady=1)
+        self.__emailEntry.grid(row=11, column=1, sticky=E, padx=20, pady=1)
         self.__emailStatusLabel.grid(row=12, column=1)
 
         # Create age Input:
         self.__ageLabel = tkinter.Label(self.__mainWindow,
-                                              text="Age:")
-        self.__ageEntry = tkinter.Entry(self.__mainWindow,
-                                              width=20)
-        self.__ageStatusLabel = tkinter.Label(self.__mainWindow, text='')
+                                        text="Age:", font=("fixedsys", 10),
+                                        fg="#ff9900")
+        self.__ageEntry = tkinter.Entry(self.__mainWindow, width=13, bg="#c2e4f0",
+                                         font=("fixedsys", 10))
+        self.__ageStatusLabel = tkinter.Label(self.__mainWindow, text='', fg="#ff9900")
+
+        self.__ageLabel.configure(background="#004c66")
+        self.__ageStatusLabel.configure(background="#004c66")
 
         # grid Widgets:
-        self.__ageLabel.grid(row=13, column=0, sticky=W, padx=7, pady=1)
-        self.__ageEntry.grid(row=13, column=1, sticky=E, padx=10, pady=1)
+        self.__ageLabel.grid(row=13, column=0, sticky=W, padx=20, pady=1)
+        self.__ageEntry.grid(row=13, column=1, sticky=E, padx=20, pady=1)
         self.__ageStatusLabel.grid(row=14, column=1)
 
         # Create buttons:
         self.__regButton = tkinter.Button(self.__buttons, text="Register",
-                                          command=self.__process, relief="groove")
+                                          command=self.__process,
+                                          relief="groove", bg="#c2e4f0",
+                                          activebackground="#d6edf5", border=0,
+                                          font=("fixedsys", 6))
         self.__cancelButton = tkinter.Button(self.__buttons, text="Cancel",
-                                             command=self.__cancel, relief="groove")
+                                             command=self.__cancel,
+                                             relief="groove", bg="#c2e4f0",
+                                             activebackground="#d6edf5", border=0,
+                                             font=("fixedsys", 6))
+        #self.__regButton.configure(background="#004c66")
+        #self.__cancelButton.configure(background="#004c66")
         # grid buttons:
-        self.__regButton.grid(row=15, column=0, sticky=E)
-        self.__cancelButton.grid(row=15, column=1, sticky=W)
+        self.__regButton.grid(row=15, column=0, sticky=E, padx=1)
+        self.__cancelButton.grid(row=15, column=1, sticky=W, padx=1)
+        
+        self.__buttons.configure(background="#004c66")
         # grid button frame:
-        self.__buttons.grid(columnspan=2, padx=7, pady=10)
+        self.__buttons.grid(columnspan=2, padx=7, pady=20)
         
 
         tkinter.mainloop()
