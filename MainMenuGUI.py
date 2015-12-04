63195011a021a2ea5f47ca3f9328fad3942f3164
jburns11 <jburns11@binghamton.edu>
2015-12-03 22:27:15 -0500

    Update MainMenuGUI.py
    new main menu guy

--- MainMenuGUI.py
+++ MainMenuGUI.py
@@ -13,7 +13,7 @@
 
 import pickle
 
-import tkinter
+from tkinter import *
 
 import tkinter.messagebox
 
@@ -35,56 +35,64 @@ class MyGUI:
         self.__mainWindow = tkinter.Tk()
         self.__mainWindow.wm_title("Main Menu")
         self.__mainWindow.configure(background = "#004c66")
+
+        #Frames
+        self.__title = tkinter.Frame(self.__mainWindow)
+        self.__buttons = tkinter.Frame(self.__mainWindow)
+        self.__title.configure(background="#004c66")
+        self.__buttons.configure(background="#004c66")
         
         #Welcome message in main window
-        self.__welcome = tkinter.Label(self.__mainWindow, text = "Hello " + \
-user + ", and welcome to the python game suite! please select a \
+        self.__welcome = tkinter.Label(self.__title, text = "Hello " + \
+user + ", and welcome to\n the python game suite! Please select a\n\
 game you would like to play!", font=("fixedsys", 15), fg="#ff9900")
         self.__welcome.configure(background = "#004c66")
         
         #Maze game button
-        self.__mazeButton = tkinter.Button(self.__mainWindow, relief = "groove",\
+        self.__mazeButton = tkinter.Button(self.__buttons, relief = "groove",\
                         text = "Maze Game", command = self.openMazeGame1,\
                         bg="#c2e4f0", activebackground="#d6edf5", border=0,
                         font=("fixedsys", 6))
         
         #Second maze game button
-        self.__mazeButton2 = tkinter.Button(self.__mainWindow, relief = "groove",\
+        self.__mazeButton2 = tkinter.Button(self.__buttons, relief = "groove",\
                            text = "Maze Game 2", command = self.openMazeGame2,
                            bg="#c2e4f0", activebackground="#d6edf5", border=0,\
                            font=("fixedsys", 6))
         
         #Hangman button
-        self.__hangManButton = tkinter.Button(self.__mainWindow, relief\
+        self.__hangManButton = tkinter.Button(self.__buttons, relief\
                 = "groove", text = "Hang Man", command = self.openHangMan,\
                 bg="#c2e4f0", activebackground="#d6edf5", border=0,\
                 font=("fixedsys", 6))
         
         #Guitar Hero button
-        self.__guitarHeroButton = tkinter.Button(self.__mainWindow,\
+        self.__guitarHeroButton = tkinter.Button(self.__buttons,\
         relief = "groove", text = "Guitar Hero", command = self.openGuitarHero\
         ,bg="#c2e4f0", activebackground="#d6edf5", border=0, \
         font=("fixedsys", 6))
 
-        self.__highScores = tkinter.Button(self.__mainWindow, relief = "groove",\
+        self.__highScores = tkinter.Button(self.__buttons, relief = "groove",\
                            text = "High Scores", command = self.viewHighScores,
                            bg="#c2e4f0", activebackground="#d6edf5", border=0,\
                            font=("fixedsys", 6))
         
         #Quit button
-        self.__quitButton = tkinter.Button(self.__mainWindow, relief = "groove",\
+        self.__quitButton = tkinter.Button(self.__buttons, relief = "groove",\
                             text = "Quit",command =  self.__mainWindow.destroy,\
                             bg="#c2e4f0", activebackground="#d6edf5", border=0,\
                            font=("fixedsys", 6))
         
         #Packing
-        self.__welcome.pack()
-        self.__mazeButton.pack()
-        self.__mazeButton2.pack()
-        self.__hangManButton.pack()
-        self.__guitarHeroButton.pack()
-        self.__highScores.pack()
-        self.__quitButton.pack()
+        self.__welcome.grid(column=1)
+        self.__mazeButton.grid(column=0, sticky=E, padx=1, pady=5)
+        self.__mazeButton2.grid(column=1, sticky=E, padx=1, pady=5)
+        self.__hangManButton.grid(column=2, sticky=E, padx=1, pady=5)
+        self.__guitarHeroButton.grid(column=3, sticky=E, padx=1, pady=5)
+        self.__highScores.grid(column=4, sticky=E, padx=1, pady=5)
+        self.__quitButton.grid(column=5, sticky=E, padx=1, pady=5)
+        self.__title.grid()
+        self.__buttons.grid()
         tkinter.mainloop()
 
     #When maze game button is hit, it will open a window, when user hits 'ok'
@@ -104,6 +112,3 @@ game you would like to play!", font=("fixedsys", 15), fg="#ff9900")
 
     def viewHighScores(self):
         HighScores.HighScores()
-
-
-
