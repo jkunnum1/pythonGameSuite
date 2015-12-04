2736bb3f05440341a15b3552091f2b9a2cc2e3c7
jburns11 <jburns11@binghamton.edu>
2015-12-03 22:25:54 -0500

    Update HighScores.py
    new high score gui

--- HighScores.py
+++ HighScores.py
@@ -12,7 +12,7 @@
 
 import pickle
 
-import tkinter
+from tkinter import *
 
 import tkinter.messagebox
 
@@ -20,79 +20,87 @@ class HighScores:
 	def __init__(self):
 		#main window
 		self.__mainWindow = tkinter.Tk()
+		self.__mainWindow.wm_title("High Scores")
+		self.__mainWindow.configure(background="#004c66")
 
 		#button frame
 		self.__buttons = tkinter.Frame(self.__mainWindow)
+		self.__buttons.configure(background="#004c66")
 
 		#scores frame
 		self.__content = tkinter.Frame(self.__mainWindow)
+		self.__content.configure(background="#004c66")
 
 		self.__filename = ""
-		self.__mazeScore = tkinter.Button(self.__buttons, relief = "groove",\
-                           text = "Maze Game", command = self.__maze1)
-		self.__mazeScore2 = tkinter.Button(self.__buttons, relief = "groove",\
-                           text = "Maze Game 2", command = self.__maze2)
-		self.__guitarScores = tkinter.Button(self.__buttons, relief = "groove",\
-                           text = "Guitar Hero", command = self.__guitarHero)
-		self.__hangman = tkinter.Button(self.__buttons, relief = "groove",\
-                           text = "Hangman", command = self.__hangman)
-		self.__quitButton = tkinter.Button(self.__buttons, relief = "groove",\
-                            text = "Quit", command =  self.__mainWindow.destroy)
+		
+		self.__mazeScore = tkinter.Button(self.__buttons, relief =\
+                "groove", text = "Maze Game", command = self.__maze1,\
+                bg="#c2e4f0", activebackground="#d6edf5", border=0,\
+                font=("fixedsys", 6))
 
-		self.__scoreLabel = tkinter.Label(self.__content, text='')
+		
+		self.__mazeScore2 = tkinter.Button(self.__buttons, relief =\
+                "groove", text = "Maze Game 2", command = self.__maze2,\
+                bg="#c2e4f0", activebackground="#d6edf5", border=0,\
+                font=("fixedsys", 6))
+
+		
+		self.__guitarScores = tkinter.Button(self.__buttons, relief =\
+                "groove",text = "Guitar Hero", command = self.__guitarHero,\
+                bg="#c2e4f0", activebackground="#d6edf5", border=0,\
+                font=("fixedsys", 6))
+
+		
+		self.__hangman = tkinter.Button(self.__buttons, relief =\
+                "groove", text = "Hangman", command = self.__hangman,\
+                bg="#c2e4f0", activebackground="#d6edf5", border=0,\
+                font=("fixedsys", 6))
+
+		
+		self.__quitButton = tkinter.Button(self.__buttons,\
+                relief = "groove", text = "Quit", command = \
+                self.__mainWindow.destroy, bg="#c2e4f0", activebackground=\
+                "#d6edf5", border=0, font=("fixedsys", 6))
+
+		self.__scoreLabel = tkinter.Label(self.__content, text='',\
+                        font=("fixedsys",10), fg="#ff9900", bg="#d6edf5")
 
 		self.__mainWindow.geometry("350x100")
 		self.__buttons.grid()
 		self.__content.grid(row=1)
-		self.__mazeScore.grid(row=0, column=0)
-		self.__mazeScore2.grid(row=0, column=2)
-		self.__guitarScores.grid(row=0, column=4)
-		self.__hangman.grid(row=0, column=6)
-		self.__quitButton.grid(row=0, column=8)
-		self.__scoreLabel.grid(rowspan=5, columnspan=5)
+		self.__mazeScore.grid(row=0, column=0, sticky=E, padx=1)
+		self.__mazeScore2.grid(row=0, column=2,sticky=E, padx=1)
+		self.__guitarScores.grid(row=0, column=4, sticky=E, padx=1)
+		self.__hangman.grid(row=0, column=6, sticky=E, padx=1)
+		self.__quitButton.grid(row=0, column=8, sticky=E, padx=1)
+		self.__scoreLabel.grid(rowspan=5, columnspan=5, sticky=E, padx=1)
 
 		tkinter.mainloop()
 
 	def __maze1(self):
 		self.__filename = "mazeGames/mazeScores.dat"
-		self.__mazeScore.config(state="active")
-		self.__mazeScore2.config(state="normal")
-		self.__guitarScores.config(state="normal")
-		self.__hangman.config(state="normal")
 		self.__score()
 
 	def __maze2(self):
 		self.__filename = "mazeGames/mazeScores2.dat"
-		self.__mazeScore.config(state="normal")
-		self.__mazeScore2.config(state="active")
-		self.__guitarScores.config(state="normal")
-		self.__hangman.config(state="normal")
 		self.__score()
 
 	def __guitarHero(self):
 		self.__filename = "guitarHero/guitarHero.dat"
-		self.__mazeScore.config(state="normal")
-		self.__mazeScore2.config(state="normal")
-		self.__guitarScores.config(state="active")
-		self.__hangman.config(state="normal")
 		self.__score()
 
 	def __hangman(self):
 		self.__filename = "Hangman/hangmanScores.dat"
-		self.__mazeScore.config(state="normal")
-		self.__mazeScore2.config(state="normal")
-		self.__guitarScores.config(state="normal")
-		self.__hangman.config(state="active")
 		self.__score()
 
 	def __score(self):
-		ranks = ""
 		try:
 			myDict = pickle.load(open(self.__filename, "rb"))
 			scoresList = []
 			for key in myDict:
 				scoresList.append(myDict[key])
 
+			ranks = ""
 			if len(myDict) > 0:
 				if len(myDict) > 3:
 					numScores = 3
@@ -109,7 +117,7 @@ class HighScores:
 					scoresList.remove(max(scoresList))
 				self.__scoreLabel.configure(text=ranks)
 			else:
-				ranks="No scores yet!"
+				tkinter.messagebox.showinfo("ERROR", "No scores yet!")
 		except IOError:
-			ranks = "Invalid Entry"
-		self.__scoreLabel.configure(text=ranks)
+			tkinter.messagebox.showinfo("ERROR", "Invalid Entry")
+
