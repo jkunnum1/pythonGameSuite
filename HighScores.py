d7d9093525605c9c1acafb43ee7c02127e713de6
William McLaughlin <wdmc85@gmail.com>
2015-12-10 18:18:17 -0500

    Fixed buttons and score background
    As title

--- HighScores.py
+++ HighScores.py
@@ -34,36 +34,36 @@ class HighScores:
 		self.__filename = ""
 		
 		self.__totalScore = tkinter.Button(self.__buttons, relief =\
-                "groove", text = "Total Scores", command = self.__total,\
+                "flat", text = "Total Scores", command = self.__total,\
                 bg="#c2e4f0", activebackground="#d6edf5", border=0,\
                 font=("fixedsys", 6))
 
 		self.__mazeScore = tkinter.Button(self.__buttons, relief =\
-                "groove", text = "Maze Game", command = self.__maze1,\
+                "flat", text = "Maze Game", command = self.__maze1,\
                 bg="#c2e4f0", activebackground="#d6edf5", border=0,\
                 font=("fixedsys", 6))
 
 		
 		self.__mazeScore2 = tkinter.Button(self.__buttons, relief =\
-                "groove", text = "Maze Game 2", command = self.__maze2,\
+                "flat", text = "Maze Game 2", command = self.__maze2,\
                 bg="#c2e4f0", activebackground="#d6edf5", border=0,\
                 font=("fixedsys", 6))
 
 		
 		self.__guitarScores = tkinter.Button(self.__buttons, relief =\
-                "groove",text = "Guitar Hero", command = self.__guitarHero,\
+                "flat",text = "Guitar Hero", command = self.__guitarHero,\
                 bg="#c2e4f0", activebackground="#d6edf5", border=0,\
                 font=("fixedsys", 6))
 
 		
 		self.__hangman = tkinter.Button(self.__buttons, relief =\
-                "groove", text = "Hangman", command = self.__hangman,\
+                "flat", text = "Hangman", command = self.__hangman,\
                 bg="#c2e4f0", activebackground="#d6edf5", border=0,\
                 font=("fixedsys", 6))
 
 		
 		self.__quitButton = tkinter.Button(self.__buttons,\
-                relief = "groove", text = "Quit", command = \
+                relief = "flat", text = "Quit", command = \
                 self.__mainWindow.destroy, bg="#c2e4f0", activebackground=\
                 "#d6edf5", border=0, font=("fixedsys", 6))
 
@@ -72,59 +72,59 @@ class HighScores:
 
 		self.__buttons.grid()
 		self.__content.grid(row=1)
-		self.__mazeScore.grid(row=0, column=0, sticky=E, padx=1)
-		self.__mazeScore2.grid(row=0, column=2,sticky=E, padx=1)
-		self.__guitarScores.grid(row=0, column=4, sticky=E, padx=1)
-		self.__hangman.grid(row=0, column=6, sticky=E, padx=1)
-		self.__totalScore.grid(row=0, column=8, sticky=E, padx=1)
-		self.__quitButton.grid(row=0, column=10, sticky=E, padx=1)
-		self.__scoreLabel.grid(rowspan=5, columnspan=5, sticky=E, padx=1)
+		self.__mazeScore.grid(row=0, column=0)
+		self.__mazeScore2.grid(row=0, column=2)
+		self.__guitarScores.grid(row=0, column=4)
+		self.__hangman.grid(row=0, column=6)
+		self.__totalScore.grid(row=0, column=8)
+		self.__quitButton.grid(row=0, column=10)
+		self.__scoreLabel.grid(rowspan=5, columnspan=5, pady=50)
 
 		tkinter.mainloop()
 
 	def __maze1(self):
 		self.__filename = "mazeGames/mazeScores.dat"
-		self.__mazeScore.configure(state="active")
-		self.__mazeScore2.configure(state="normal")
-		self.__guitarScores.configure(state="normal")
-		self.__hangman.configure(state="normal")
-		self.__totalScore.configure(state="normal")
+		self.__mazeScore.configure(bg="#ebf6fa")
+		self.__mazeScore2.configure(bg="#addbeb")
+		self.__guitarScores.configure(bg="#addbeb")
+		self.__hangman.configure(bg="#addbeb")
+		self.__totalScore.configure(bg="#addbeb")
 		self.__score()
 
 	def __maze2(self):
 		self.__filename = "mazeGames/mazeScores2.dat"
-		self.__mazeScore.configure(state="normal")
-		self.__mazeScore2.configure(state="active")
-		self.__guitarScores.configure(state="normal")
-		self.__hangman.configure(state="normal")
-		self.__totalScore.configure(state="normal")
+		self.__mazeScore.configure(bg="#addbeb")
+		self.__mazeScore2.configure(bg="#ebf6fa")
+		self.__guitarScores.configure(bg="#addbeb")
+		self.__hangman.configure(bg="#addbeb")
+		self.__totalScore.configure(bg="#addbeb")
 		self.__score()
 
 	def __guitarHero(self):
 		self.__filename = "guitarHero/guitarHero.dat"
-		self.__mazeScore.configure(state="normal")
-		self.__mazeScore2.configure(state="normal")
-		self.__guitarScores.configure(state="active")
-		self.__hangman.configure(state="normal")
-		self.__totalScore.configure(state="normal")
+		self.__mazeScore.configure(bg="#addbeb")
+		self.__mazeScore2.configure(bg="#addbeb")
+		self.__guitarScores.configure(bg="#ebf6fa")
+		self.__hangman.configure(bg="#addbeb")
+		self.__totalScore.configure(bg="#addbeb")
 		self.__score()
 
 	def __hangman(self):
 		self.__filename = "Hangman/hangmanScores.dat"
-		self.__mazeScore.configure(state="normal")
-		self.__mazeScore2.configure(state="normal")
-		self.__guitarScores.configure(state="normal")
-		self.__hangman.configure(state="active")
-		self.__totalScore.configure(state="normal")
+		self.__mazeScore.configure(bg="#addbeb")
+		self.__mazeScore2.configure(bg="#addbeb")
+		self.__guitarScores.configure(bg="#addbeb")
+		self.__hangman.configure(bg="#ebf6fa")
+		self.__totalScore.configure(bg="#addbeb")
 		self.__score()
 
 	def __total(self):
 		self.__filename = "users.dat"
-		self.__mazeScore.configure(state="normal")
-		self.__mazeScore2.configure(state="normal")
-		self.__guitarScores.configure(state="normal")
-		self.__hangman.configure(state="normal")
-		self.__totalScore.configure(state="active")
+		self.__mazeScore.configure(bg="#addbeb")
+		self.__mazeScore2.configure(bg="#addbeb")
+		self.__guitarScores.configure(bg="#addbeb")
+		self.__hangman.configure(bg="#addbeb")
+		self.__totalScore.configure(bg="#ebf6fa")
 		self.__loadTotal()
 
 	def __loadTotal(self):
@@ -135,8 +135,12 @@ class HighScores:
 			for key in myDict:
 				scoresList.append(myDict[key][-1])
 
-			for i in range(3):
-				try:
+			if len(myDict) > 0:
+				if len(myDict) > 3:
+					numScores = 3
+				else:
+					numScores = 1
+				for i in range(numScores):
 					highest = max(scoresList)
 					found = False
 					for key in myDict:
@@ -146,11 +150,11 @@ class HighScores:
 					ranks += highKey +  "     :    " + str(highest) + "\n"
 					myDict.pop(highKey)
 					scoresList.remove(max(scoresList))
-				except:
-					ranks += "None     :    0\n"
+			else:
+				ranks = "No scores yet!"
 		except IOError:
 			ranks = "Invalid Entry"
-		self.__scoreLabel.configure(text=ranks, justify=LEFT)
+		self.__scoreLabel.configure(text=ranks)
 
 	def __score(self):
 		ranks = ""
@@ -160,8 +164,12 @@ class HighScores:
 			for key in myDict:
 				scoresList.append(myDict[key])
 
-			for i in range(3):
-				try:
+			if len(myDict) > 0:
+				if len(myDict) > 3:
+					numScores = 3
+				else:
+					numScores = 1
+				for i in range(numScores):
 					highest = max(scoresList)
 					found = False
 					for key in myDict:
@@ -170,9 +178,9 @@ class HighScores:
 							found = True
 					ranks += highKey +  "     :    " + str(myDict.pop(highKey)) + "\n"
 					scoresList.remove(max(scoresList))
-				except:
-					ranks += "None     :    0\n"
+			else:
+				ranks = "No scores yet!"
 		except IOError:
 			ranks = "Invalid Entry"
-		self.__scoreLabel.configure(text=ranks, justify=LEFT, bg = "#c2e4f0")
+		self.__scoreLabel.configure(text=ranks)
 
