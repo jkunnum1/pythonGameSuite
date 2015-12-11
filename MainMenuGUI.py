7f5c2cd602144fec8c8a2c142b59ab63ac19157f
jburns11 <jburns11@binghamton.edu>
2015-12-10 19:08:23 -0500

    pep8
    pep8

--- MainMenuGUI.py
+++ MainMenuGUI.py
@@ -23,77 +23,96 @@ from mazeGames import NewMaze
 
 from guitarHero import GuitarHero
 
-from Hangman import hangmanV5
+from Hangman import Hangman
 
 from slotmachine import SlotMachine
 
 import HighScores
 
+
 class MyGUI:
     def __init__(self):
-        #Imports online user, and saves username
+        # Imports online user, and saves username
         inFile = pickle.load(open("userOnline.dat", "rb"))
         user = inFile[0]
-        
-        #Main window
+
+        # Main window
         self.__mainWindow = tkinter.Tk()
         self.__mainWindow.wm_title("Main Menu")
-        self.__mainWindow.configure(background = "#004c66")
+        self.__mainWindow.configure(background="#004c66")
 
-        #Frames
+        # Frames
         self.__title = tkinter.Frame(self.__mainWindow)
         self.__buttons = tkinter.Frame(self.__mainWindow)
         self.__title.configure(background="#004c66")
         self.__buttons.configure(background="#004c66")
-        
-        #Welcome message in main window
-        self.__welcome = tkinter.Label(self.__title, text = "Hello " + \
-user + ", and welcome to\n the python game suite! Please select a\n\
-game you would like to play!", font=("fixedsys", 15), fg="#ff9900")
-        self.__welcome.configure(background = "#004c66")
-        
-        #Maze game button
-        self.__mazeButton = tkinter.Button(self.__buttons, relief = "groove",\
-                        text = "Maze Game", command = self.openMazeGame1,\
-                        bg="#c2e4f0", activebackground="#d6edf5", border=0,
-                        font=("fixedsys", 6))
-        
-        #Second maze game button
-        self.__mazeButton2 = tkinter.Button(self.__buttons, relief = "groove",\
-                           text = "Maze Game 2", command = self.openMazeGame2,
-                           bg="#c2e4f0", activebackground="#d6edf5", border=0,\
-                           font=("fixedsys", 6))
-        
-        #Hangman button
-        self.__hangManButton = tkinter.Button(self.__buttons, relief\
-                = "groove", text = "Hang Man", command = self.openHangMan,\
-                bg="#c2e4f0", activebackground="#d6edf5", border=0,\
-                font=("fixedsys", 6))
-        
-        #Guitar Hero button
-        self.__guitarHeroButton = tkinter.Button(self.__buttons,\
-        relief = "groove", text = "Guitar Hero", command = self.openGuitarHero\
-        ,bg="#c2e4f0", activebackground="#d6edf5", border=0, \
-        font=("fixedsys", 6))
 
-        #Slotmachine button
-        self.__slotButton = tkinter.Button(self.__buttons, relief\
-                = "groove", text = "Slot machine", command = self.openSlots,\
-                bg="#c2e4f0", activebackground="#d6edf5", border=0,\
-                font=("fixedsys", 6))
+        # Welcome message in main window
+        self.__welcome = tkinter.Label(self.__title, text="Hello " +
+                                       user+", and welcome to\n the python\
+                                       game suite! Please select a\n game you\
+                                       would like to play!",
+                                       font=("fixedsys", 15), fg="#ff9900")
+        self.__welcome.configure(background="#004c66")
 
-        self.__highScores = tkinter.Button(self.__buttons, relief = "groove",\
-                           text = "High Scores", command = self.viewHighScores,
-                           bg="#c2e4f0", activebackground="#d6edf5", border=0,\
-                           font=("fixedsys", 6))
-        
-        #Quit button
-        self.__quitButton = tkinter.Button(self.__buttons, relief = "groove",\
-                            text = "Quit",command =  self.__mainWindow.destroy,\
-                            bg="#c2e4f0", activebackground="#d6edf5", border=0,\
-                           font=("fixedsys", 6))
-        
-        #Packing
+        # Maze game button
+        self.__mazeButton = tkinter.Button(self.__buttons, relief="groove",
+                                           text="Maze Game",
+                                           command=self.openMazeGame1,
+                                           bg="#c2e4f0",
+                                           activebackground="#d6edf5",
+                                           border=0, font=("fixedsys", 6))
+
+        # Second maze game button
+        self.__mazeButton2 = tkinter.Button(self.__buttons, relief="groove",
+                                            text="Maze Game 2",
+                                            command=self.openMazeGame2,
+                                            bg="#c2e4f0",
+                                            activebackground="#d6edf5",
+                                            border=0, font=("fixedsys", 6))
+
+        # Hangman button
+        self.__hangManButton = tkinter.Button(self.__buttons, relief="groove",
+                                              text="Hang Man",
+                                              command=self.openHangMan,
+                                              bg="#c2e4f0",
+                                              activebackground="#d6edf5",
+                                              border=0, font=("fixedsys", 6))
+
+        # Guitar Hero button
+        self.__guitarHeroButton = tkinter.Button(self.__buttons,
+                                                 relief="groove",
+                                                 text="Guitar Hero",
+                                                 command=self.openGuitarHero,
+                                                 bg="#c2e4f0",
+                                                 activebackground="#d6edf5",
+                                                 border=0,
+                                                 font=("fixedsys", 6))
+
+        # Slotmachine button
+        self.__slotButton = tkinter.Button(self.__buttons, relief="groove",
+                                           text="Slot machine",
+                                           command=self.openSlots,
+                                           bg="#c2e4f0",
+                                           activebackground="#d6edf5",
+                                           border=0, font=("fixedsys", 6))
+
+        self.__highScores = tkinter.Button(self.__buttons, relief="groove",
+                                           text="High Scores",
+                                           command=self.viewHighScores,
+                                           bg="#c2e4f0",
+                                           activebackground="#d6edf5",
+                                           border=0, font=("fixedsys", 6))
+
+        # Quit button
+        self.__quitButton = tkinter.Button(self.__buttons, relief="groove",
+                                           text="Quit",
+                                           command=self.__mainWindow.destroy,
+                                           bg="#c2e4f0",
+                                           activebackground="#d6edf5",
+                                           border=0, font=("fixedsys", 6))
+
+        # Packing
         self.__welcome.grid(column=1)
         self.__mazeButton.grid(column=0, sticky=E, padx=1, pady=5)
         self.__mazeButton2.grid(column=1, sticky=E, padx=1, pady=5)
@@ -106,17 +125,17 @@ game you would like to play!", font=("fixedsys", 15), fg="#ff9900")
         self.__buttons.grid()
         tkinter.mainloop()
 
-    #When maze game button is hit, it will open a window, when user hits 'ok'
-    #The game will start
+    # When maze game button is hit, it will open a window, when user hits 'ok'
+    # The game will start
     def openMazeGame1(self):
         Maze.Maze()
 
     def openMazeGame2(self):
         NewMaze.NewMaze()
 
-    #Same as above but with Hang man
+    # Same as above but with Hang man
     def openHangMan(self):
-        hangmanV5.Hangman()
+        Hangman.Hangman()
 
     def openGuitarHero(self):
         GuitarHero.GuitarHero()
